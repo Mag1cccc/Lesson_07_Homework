9b0ba1748ba51345ca1d65a3d82004049d730d43
amiko morgoshia <morgoshiaaamiko@gmail.com>
2026-06-04 22:06:52 +0400

    feat: implement calculate square difference logic

--- Homework_7/Program.cs
+++ Homework_7/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CalculateSquareDifference();
+        }
+
+        public static void CalculateSquareDifference()
+        {
+            Console.WriteLine("Enter radius of the circle: ");
+
+            if (int.TryParse(Console.ReadLine(), out var radius))
+            {
+                var largeSquareArea = 4 * (radius * radius);
+                var smallSquareArea = 2 * (radius * radius);
+                var difference = largeSquareArea - smallSquareArea;
+                Console.WriteLine($"The difference is: {difference}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
         }
     }
 }
